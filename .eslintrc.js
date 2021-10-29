5284d7d5c2f5ce38a2137a9190e4742263ca2c00
kyle <704334497@qq.com>
2021-10-29 18:12:55 +0800

    install husky redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
-  settings: {
-    react: {
-      version: 'detect',
+    env: {
+        browser: true,
+        es2021: true
     },
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'no-debugger': 'off',
-    '@typescript-eslint/no-this-alias': ['off'],
-    '@typescript-eslint/no-invalid-this': ['off'],
-    '@typescript-eslint/no-require-imports': ['off'],
-  },
-  ignorePatterns: ['stories/*', 'src/stories/*'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'no-debugger': 'off',
+        '@typescript-eslint/no-this-alias': ['off'],
+        '@typescript-eslint/no-invalid-this': ['off'],
+        '@typescript-eslint/no-require-imports': ['off']
+    },
+    ignorePatterns: ['stories/*', 'src/stories/*', 'src/react-app-env.d.ts']
 };
