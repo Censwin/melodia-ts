6b5cce0da67446a8f8be155802f9bbc31f2195ab
kyle <704334497@qq.com>
2021-11-26 18:20:19 +0800

    finish rank list

--- src/application/Rank/index.tsx
+++ src/application/Rank/index.tsx
@@ -1,42 +1,97 @@
 /*
  * @Date: 2021-11-24 15:32:17
  * @LastEditors: k200c
- * @LastEditTime: 2021-11-25 18:04:49
+ * @LastEditTime: 2021-11-26 18:19:25
  * @Description: 排行榜
  * @FilePath: \melodia-ts\src\application\Rank\index.tsx
  */
 import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { useHistory } from 'react-router';
-import { Header } from '../../baseUI';
+import { Header, Scroll } from '../../baseUI';
 import * as ActionType from './store/constans';
 import { IApplicationState } from '../../store/reducers';
 import { useDispatch, useSelector } from 'react-redux';
+import { filterRankList } from '../../utils/tools';
+import { Card, Icon } from '../../components';
+import LazyLoad, { forceCheck } from 'react-lazyload';
+import defaultImg from './../../assets/img/defaultmusic.png';
+
 const Rank = () => {
   const [showStatus, setShowStatus] = useState(true);
   const history = useHistory();
   const nodeRef = useRef(null);
   const dispatch = useDispatch();
   const { rankList } = useSelector((state: IApplicationState) => state.Rank);
-  console.log(rankList);
+  type ScrollComponentType = React.ElementRef<typeof Scroll>;
+  const ScrollRef = useRef<ScrollComponentType>(null);
   useEffect(() => {
     dispatch({ type: ActionType.GET_RANKLIST });
   }, []);
-  const findIndex = useCallback((_list) => {
-    for (let i = 0; i < _list.length; i++) {
-      if (_list[i].tracks.length && !_list[i + 1].tracks.length) {
-        return i + 1;
-      }
-    }
+
+  const [officialList, choicenessList, musicstyleList, globalList, specialList] = useMemo(() => {
+    return filterRankList(rankList);
+  }, [rankList.length]);
+  useEffect(() => {
+    ScrollRef.current?.refresh();
+  });
+  const enterDetail = useCallback((id) => {
+    history.push(`/album/${id}`);
   }, []);
-  const [globalStartIndex, officialList, globalList] = useMemo(() => {
-    let index = findIndex(rankList);
-    let list1 = rankList.slice(0, index);
-    let list2 = rankList.slice(index);
-    return [index, list1, list2];
-  }, rankList);
-  console.log(officialList);
-  console.log(globalList);
+  const renderSongList = useCallback(
+    (list) => (
+      <ul className="song-list-wrapper">
+        {list.map((item: any, index: number) => {
+          return (
+            <li key={index}>
+              {index + 1}. {item.first} <span className="singer-name"> — {item.second}</span>
+            </li>
+          );
+        })}
+      </ul>
+    ),
+    [rankList.length]
+  );
+  const RenderOfficial = useCallback(
+    () => (
+      <ul className="official-wrapper">
+        {officialList.map((item) => {
+          return (
+            <li key={item.id} className="official-item" onClick={() => enterDetail(item.id)}>
+              <div className="img_wrapper">
+                <LazyLoad placeholder={<img width="100%" height="100%" src={defaultImg} />}>
+                  <img src={item.coverImgUrl} alt="" />
+                </LazyLoad>
+              </div>
+              <span className="update_frequecy">{item.updateFrequency}</span>
+              {renderSongList(item.tracks)}
+            </li>
+          );
+        })}
+      </ul>
+    ),
+    [rankList.length]
+  );
+  const RenderRankList = useCallback(
+    (list) => (
+      <ul className="rank-list-wrapper">
+        {list.map((item: any) => {
+          return (
+            <li key={item.id} className="rank-item" onClick={() => enterDetail(item.id)}>
+              <LazyLoad placeholder={<img width="100%" height="100%" src={defaultImg} />}>
+                <img src={item.coverImgUrl} alt="" />
+              </LazyLoad>
+              <div className="decorate" />
+              <span className="update_frequecy">{item.updateFrequency}</span>
+              <Icon className="playicon" icon="play" />
+              <p className="rank-name">{item.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    ),
+    [rankList.length]
+  );
   return (
     <CSSTransition
       in={showStatus}
@@ -48,7 +103,26 @@ const Rank = () => {
       nodeRef={nodeRef}
     >
       <div className="rank-container" ref={nodeRef}>
-        <Header title="排行榜" handleClick={() => setShowStatus(false)} />
+        <Header
+          style={{ background: '#fff' }}
+          title="排行榜"
+          handleClick={() => setShowStatus(false)}
+        />
+        <section className="scroll-window-wrapper">
+          <Scroll ref={ScrollRef} onScroll={forceCheck}>
+            <div>
+              <div className="official-title">
+                <Icon icon="music" />
+                官方榜
+              </div>
+              {RenderOfficial()}
+              <Card title="精选榜">{RenderRankList(choicenessList)}</Card>
+              <Card title="曲风榜">{RenderRankList(musicstyleList)}</Card>
+              <Card title="全球榜">{RenderRankList(globalList)}</Card>
+              <Card title="特色榜">{RenderRankList(specialList)}</Card>
+            </div>
+          </Scroll>
+        </section>
       </div>
     </CSSTransition>
   );
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-11-17 14:47:57
  * @LastEditors: k200c
- * @LastEditTime: 2021-11-25 17:54:29
+ * @LastEditTime: 2021-11-26 15:31:56
  * @Description:
  * @FilePath: \melodia-ts\src\utils\tools.ts
  */
@@ -27,4 +27,62 @@ export const isEmptyObject = (obj: any) => {
     obj.constructor === Array ||
     (obj.constructor === Object && Object.keys(obj).length === 0)
   );
+};
+
+export const filterRankList = (list: Array<any>) => {
+  // const recommend = ['云音乐欧美热歌榜', '云音乐韩语榜', '美国Billboard榜']; // 推荐榜
+  const choiceness = ['黑胶VIP爱听榜', '网络热歌榜', 'KTV唛榜']; // 精选榜
+  const musicstyle = [
+    '云音乐电音榜',
+    '云音乐ACG榜',
+    '云音乐说唱榜',
+    '云音乐摇滚榜',
+    '云音乐民谣榜',
+    '云音乐国电榜',
+    '云音乐古典榜',
+    '云音乐古风榜',
+    '中文DJ榜'
+  ]; // 曲风榜
+  const global = [
+    '美国Billboard榜',
+    'UK排行榜周榜',
+    '日本Oricon榜',
+    '法国 NRJ Vos Hits 周榜',
+    '俄罗斯top hit流行音乐榜',
+    '云音乐欧美新歌榜',
+    '云音乐欧美热歌榜',
+    '云音乐日语榜',
+    '云音乐韩语榜',
+    '俄语榜',
+    '越南语榜'
+  ]; // 全球榜
+  let index;
+  let officialList = []; // 官方榜
+  let choicenessList = [];
+  let musicstyleList = [];
+  let globalList = [];
+  let specialList = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].tracks.length && !list[i + 1].tracks.length) {
+      index = i + 1;
+      break;
+    }
+  }
+  officialList = list.slice(0, index);
+  let list2 = list.slice(index);
+  for (const cur of list2) {
+    if (choiceness.some((name) => name === cur.name)) {
+      choicenessList.push(cur);
+      continue;
+    } else if (musicstyle.some((name) => name === cur.name)) {
+      musicstyleList.push(cur);
+      continue;
+    } else if (global.some((name) => name === cur.name)) {
+      globalList.push(cur);
+      continue;
+    } else {
+      specialList.push(cur);
+    }
+  }
+  return [officialList, choicenessList, musicstyleList, globalList, specialList];
 };
